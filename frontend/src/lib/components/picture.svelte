ceaaff1f7be621fb88dfbd3dce9282a775c48e24
Dev Ekkx <devekkx@gmail.com>
2025-09-18 13:38:32 +0000

    feature: create a picture component

---
+++ frontend/src/lib/components/picture.svelte
@@ -0,0 +1,8 @@
+<script lang="ts">
+	const { src, source, alt, ...rest } = $props();
+</script>
+
+<picture>
+	<source srcset={source} type="image/webp">
+	<img {...rest} alt={alt} src={src} srcset={source} />
+</picture>
--- frontend/src/routes/_home/flash-sales.svelte
+++ frontend/src/routes/_home/flash-sales.svelte
@@ -1,10 +1,13 @@
 <script lang="ts">
-	import FlashSalesImage from '$lib/assets/images/category16.jpg';
+	import FlashSalesImageJp from '$lib/assets/images/category16.jpg';
+	import FlashSalesImageWp from '$lib/assets/images/category16.webp';
 	import { Button } from '$lib/components/ui/button';
+	import Picture from '$lib/components/picture.svelte';
 </script>
 
 <section class="grid grid-cols-1 md:grid-cols-2 md:max-h-[28rem] g-mt rounded-lg overflow-hidden">
-	<img alt="flash sales" class="object-cover w-full max-h-[19rem] lg:max-h-auto" src={FlashSalesImage}>
+	<Picture alt="flash sales" class="object-cover w-full max-h-[19rem] lg:max-h-auto" source={FlashSalesImageWp}
+					 src={FlashSalesImageJp} />
 	<div class="bg-muted-foreground px-6 py-10 flex flex-col gap-10 lg:items-start lg:justify-center **:text-white">
 		<div class="flex flex-col gap-2">
 			<span class="font-bold text-2xl">FLASH SALE: 25% OFF All Living Room Furniture</span>
