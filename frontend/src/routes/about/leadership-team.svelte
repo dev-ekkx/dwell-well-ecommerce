1e832fdffeafce4c64b114b9b4f36c54225f3de8
Dev Ekkx <devekkx@gmail.com>
2025-09-25 09:07:34 +0000

    feature: create leadership team component

--- frontend/src/routes/about/leadership-team.svelte
+++ frontend/src/routes/about/leadership-team.svelte
@@ -1,11 +1,26 @@
 <script lang="ts">
 	import CEOImage from '$lib/assets/images/ceo.jpeg';
+	import Person1Image from '$lib/assets/images/person1.jpg';
+	import Person2Image from '$lib/assets/images/person2.jpg';
+	import Person3Image from '$lib/assets/images/person3.jpg';
 
 	const managers = [
 		{
 			name: 'Emmanuel Komla Kpendo',
-			position: 'Founder & CEO',
+			position: 'Founder & Chief Executive Officer',
 			image: CEOImage
+		}, {
+			name: 'Dela Ricch',
+			position: 'Chief Operations Office',
+			image: Person1Image
+		}, {
+			name: 'Jasmine Nun',
+			position: 'Chief Financial Officer',
+			image: Person2Image
+		}, {
+			name: 'Ruth Naomi',
+			position: 'Chief Design Officer',
+			image: Person3Image
 		}
 	];
 </script>
@@ -19,9 +34,9 @@
 	<div class="grid grid-cols-1 gap-4 md:grid-cols-2 lg:grid-cols-4">
 		{#each managers as manager (manager.name)}
 			<div class="h-[28rem] lg:h-[23rem] relative rounded-lg overflow-hidden">
-				<img alt={manager.name} src={manager.image} class="h-full object-cover" />
+				<img alt={manager.name} src={manager.image} class="h-full w-full object-cover object-top" />
 				<div
-					class="absolute flex flex-col **:text-white justify-end px-4 py-3 gap-1 bg-gradient-to-b from-transparent via-transparent to-black/80 bottom-0 left-0 w-full h-5/12">
+					class="absolute flex flex-col **:text-white justify-end px-4 py-3 gap-1 bg-gradient-to-b from-transparent to-black/90 bottom-0 left-0 w-full h-3/12">
 					<span class="font-semibold">{manager.name}</span>
 					<span class="text-xs font-light">{manager.position}</span>
 				</div>
