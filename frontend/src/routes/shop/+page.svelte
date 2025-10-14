c7503b33e2ea3a5c6710434bfde4c79d1d8d1f73
Dev Ekkx <devekkx@gmail.com>
2025-10-14 20:50:59 +0000

    feature: install star rating component

--- frontend/src/routes/shop/+page.svelte
+++ frontend/src/routes/shop/+page.svelte
@@ -1,6 +1,8 @@
 <script lang="ts">
 	import type { PageProps } from './$types';
+	import { formatNumberWithCommas } from '$lib/utils';
 	import FiltersAndSort from './filter-and-sort.svelte';
+	import ProductCard from './product-card.svelte';
 
 	const { data }: PageProps = $props();
 	const seoData = data.seo;
@@ -17,8 +19,43 @@
 <section class="g-px flex gap-6">
 
 	<!--	Filter and Sort -->
-	<FiltersAndSort {filters} />
+	<div class="hidden xl:block">
+		<FiltersAndSort {filters} />
+	</div>
 
 	<!--	main content -->
+	<div class="flex flex-col gap-6 flex-1">
+
+		<!-- Page heading -->
+		<div class="flex items-center gap-2">
+			<h4 class="font-bold text-2xl leading-8">Search results for beds</h4>
+			<span class="text-muted-foreground">{formatNumberWithCommas(1500)}+ items found</span>
+		</div>
+
+		<!--	Product items -->
+		<section class="grid grid-cols-2 gap-4 sm:gap-6 md:gap-8 md:grid-cols-3 xl:grid-cols-4">
+			<ProductCard />
+			<ProductCard />
+			<ProductCard />
+			<ProductCard />
+			<ProductCard />
+			<ProductCard />
+			<ProductCard />
+			<ProductCard />
+			<ProductCard />
+			<ProductCard />
+			<ProductCard />
+			<ProductCard />
+			<ProductCard />
+			<ProductCard />
+			<ProductCard />
+			<ProductCard />
+			<ProductCard />
+			<ProductCard />
+			<ProductCard />
+			<ProductCard />
+			<ProductCard />
+		</section>
+	</div>
 </section>
 
---
+++ frontend/src/routes/shop/product-card.svelte
@@ -0,0 +1,51 @@
+<script lang="ts">
+	import ProductImage from '$lib/assets/images/auth-bg.webp';
+	import { formatNumberWithCommas } from '$lib/utils';
+	import { type ConfigI, StarRating } from '@dev-ekkx/svelte-star-rating';
+
+	const config = $state<ConfigI>({
+		readonly: true,
+		maxVal: 5,
+		minVal: 0,
+		step: 0.1,
+		numOfStars: 5,
+		starConfig: {
+			size: 12,
+			filledColor: '#F98416',
+			unfilledColor: '#5D5D5D'
+		},
+		styles: {
+			containerStyles: 'width: max-content',
+			starStyles: 'gap: 0.1rem'
+		}
+	});
+
+	let value = $state(4.8);
+
+
+</script>
+
+<div class="flex flex-col gap-4 relative group">
+	<!--	add to cart button-->
+	<button class="absolute z-10 top-4 right-2 rounded-full bg-primary h-8 w-8 flex items-center justify-center">ATC
+	</button>
+
+	<div class="h-[11rem] md:h-[12rem] rounded-lg overflow-clip">
+		<img alt=""
+				 class="object-cover group-hover:scale-110 w-full h-full transition-all duration-200 ease-linear"
+				 src={ProductImage}>
+	</div>
+
+	<div class="flex flex-col gap-2">
+		<span class="font-semibold text-xl leading-7">Product name</span>
+		<span class="flex items-center gap-1">
+			<StarRating bind:value {config} />
+	<span class="text-muted-foreground">4.8 (120 Reviews)</span>
+		</span>
+		<span class="flex items-center gap-1 font-bold text-xl leading-7">
+			<span class="text-muted-foreground line-through">${formatNumberWithCommas(400)}</span>
+			<span class="">${formatNumberWithCommas(299)} </span>
+		</span>
+	</div>
+
+</div>
