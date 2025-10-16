0794f16dbae9da9d619f7914144353f76f9cdd87
Dev Ekkx <devekkx@gmail.com>
2025-10-16 11:49:31 +0000

    feature: install sheep component

--- frontend/src/routes/shop/+page.svelte
+++ frontend/src/routes/shop/+page.svelte
@@ -15,6 +15,14 @@
 		PrevButton as PaginationPrevButton,
 		Root as PaginationRoot
 	} from '$lib/components/ui/pagination/index.js';
+
+	import {
+		Content as DrawerContent,
+		Overlay as DrawerOverlay,
+		Root as DrawerRoot,
+		Trigger as DrawerTrigger
+	} from '$lib/components/ui/drawer/index.js';
+
 	import CaretIcon from '$lib/assets/caret-up.svg';
 	import FilterIcon from '$lib/assets/filter.svg';
 	import { MediaQuery } from 'svelte/reactivity';
@@ -27,6 +35,7 @@
 	const isMobile = $derived(mediaQuery.current);
 
 	// Page state
+	let isDrawerOpen = $state(false);
 	const itemsPerPageOptions = $state([...ITEMS_PER_PAGE_OPTIONS]);
 	let itemsPerPage = $state('10');
 	let currentPage = $state(1);
@@ -51,21 +60,27 @@
 
 <div class="flex flex-col gap-10">
 	<section class="g-px flex gap-6">
-		<!--	Filter and Sort -->
+		<!-- Desktop Filter and Sort -->
 		<div class="hidden lg:block">
 			<FiltersAndSort {filters} />
 		</div>
 
 		<!--	main content -->
 		<div class="flex flex-col gap-6 flex-1">
-
-			<!--	Filter toggle button -->
-			{#if isMobile}
-				<button class="flex items-center gap-1 text-primary cursor-pointer font-semibold">
+			<!--	Mobile Filter and Sort Drawer -->
+			<DrawerRoot bind:open={isDrawerOpen}>
+				<DrawerTrigger class="flex items-center gap-1 text-primary cursor-pointer font-semibold">
 					<img alt="filter" src={FilterIcon}>
 					<span>Toggle filters</span>
-				</button>
-			{/if}
+				</DrawerTrigger>
+				<DrawerContent class="w-max h-max bg-red-600 p-4 overflow-y-scroll">
+					<section class="py-4 bg-blue-600">
+						<FiltersAndSort {filters} />
+					</section>
+				</DrawerContent>
+
+				<DrawerOverlay class="backdrop-blur-xs" />
+			</DrawerRoot>
 
 			<!-- Page heading -->
 			<div class="flex items-center gap-2">
