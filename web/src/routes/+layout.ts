158bea266177991b33adebf2534bdf4d02aa5377
Dev Ekkx <devekkx@gmail.com>
2025-10-24 09:17:29 +0000

    chore: configure storyblok

--- web/src/routes/+layout.ts
+++ web/src/routes/+layout.ts
@@ -1,18 +1,32 @@
-import type { LayoutLoad } from "./$types";
-import type { FooterI } from "$lib/interfaces";
+import type {LayoutLoad} from "./$types";
+import {apiPlugin, storyblokInit, useStoryblokApi} from "@storyblok/svelte";
 
 export const load: LayoutLoad = async ({ fetch }) => {
-	try {
-		const footerData = await fetch(import.meta.env.VITE_CMS_URL + "/api/footer?populate=all");
-		const data = (await footerData.json()).data as FooterI;
+	// try {
+	// 	const footerData = await fetch(import.meta.env.VITE_CMS_URL + "/api/footer?populate=all");
+	// 	const data = (await footerData.json()).data as FooterI;
+    //
+	// 	return {
+	// 		footer: data
+	// 	};
+	// } catch (error) {
+	// 	console.error("Error loading footer data: ", error);
+	// 	return {
+	// 		footer: null
+	// 	};
+	// }
 
-		return {
-			footer: data
-		};
-	} catch (error) {
-		console.error("Error loading footer data: ", error);
-		return {
-			footer: null
-		};
-	}
+    storyblokInit({
+        accessToken: import.meta.env.VITE_STORYBLOK_DELIVERY_API_TOKEN,
+        apiOptions: {
+            region: 'eu',
+        },
+        use: [apiPlugin],
+    });
+
+    const storyblokAPI = await useStoryblokApi();
+
+    return {
+        storyblokAPI,
+    };
 };
