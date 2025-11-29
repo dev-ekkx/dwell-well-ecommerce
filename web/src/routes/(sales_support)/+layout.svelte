8dd7af97d02c2619dce9b6d831780a0407c38a48
Dev Ekkx <devekkx@gmail.com>
2025-11-29 19:20:28 +0000

    feature: create sales support header component

--- web/src/routes/(sales_support)/+layout.svelte
+++ web/src/routes/(sales_support)/+layout.svelte
@@ -1,11 +1,13 @@
 <script lang="ts">
+	import Header from "./header.svelte";
 	import Sidebar from "./sidebar.svelte";
 	const { children } = $props();
 </script>
 
 <div class="flex gap-4 bg-muted">
 	<Sidebar />
-	<div class="flex flex-col">
+	<div class="flex flex-col w-full">
+        <Header />
 		<main>
 			{@render children()}
 		</main>
---
+++ web/src/routes/(sales_support)/header.svelte
@@ -0,0 +1,25 @@
+<script lang="ts">
+	import { page } from "$app/state";
+	import { AvatarFallback, AvatarImage, Root as AvatarRoot } from "$lib/components/ui/avatar";
+	import { createInitial } from "$lib/utils";
+	import { BellIcon } from "@lucide/svelte";
+
+	const userData = $derived(page.data.user);
+
+$inspect(userData);
+</script>
+
+<header class="flex items-center justify-end h-[4.5rem] bg-white p-4">
+    <div class="flex items-center gap-4">
+        <button class="cursor-pointer bg-primary-foreground p-2 rounded-full relative">
+            <BellIcon />
+            <span class="absolute top-0 right-0 w-2 h-2 bg-primary rounded-full"></span>
+        </button>
+
+        	<AvatarRoot class="size-10">
+					<AvatarImage src={userData?.image} alt={userData?.name} />
+					<AvatarFallback class="w-full">{createInitial(userData?.name)}</AvatarFallback>
+				</AvatarRoot>
+    </div>
+
+</header>
