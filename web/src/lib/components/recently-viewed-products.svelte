a4392caa727118f7ae12eab2a4209167b5b2b4cf
Dev Ekkx <devekkx@gmail.com>
2025-12-27 21:42:10 +0000

    refactor: update recently viewed products component

--- web/src/lib/components/recently-viewed-products.svelte
+++ web/src/lib/components/recently-viewed-products.svelte
@@ -1,9 +1,9 @@
 <script lang="ts">
-	import ProductCard from "./product-card.svelte";
-	import { recentlyViewedStore } from "$lib/store/recently-viewed-store.svelte";
 	import { goto } from "$app/navigation";
 	import { resolve } from "$app/paths";
+	import { recentlyViewedStore } from "$lib/store/recently-viewed-store.svelte";
 	import { cn } from "$lib/utils";
+	import ProductCard from "./product-card.svelte";
 
 	const { className }: { className?: string } = $props();
 	const recentlyViewedProducts = $derived(recentlyViewedStore.recentlyViewedProducts);
@@ -15,7 +15,6 @@
 <div class={cn("flex flex-col gap-6", className)}>
 	<span class="text-2xl leading-8 font-semibold">Recently Viewed</span>
 
-	<!--	<section class="flex w-full flex-col gap-4 overflow-x-auto md:flex-row md:gap-6 xl:gap-10">-->
 	<section
 		class="grid grid-cols-1 gap-4 sm:grid-cols-2 md:gap-6 lg:grid-cols-3 lg:gap-8 xl:grid-cols-4 2xl:grid-cols-5"
 	>
